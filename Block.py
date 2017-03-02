f4a138e2b5d5d7e3faffd3b0dcd524d524736b16
FundamentalEq <bloodspain@gmail.com>
2017-03-03 01:22:02 +0530

    Added UpdateBackward to update block state on removal of a move

--- Block.py
+++ Block.py
@@ -14,24 +14,47 @@ class Block() :
         self.BlockUtility = 4*lineState.StateUtility[0]
 
   # [ Current State][ Cell No ][ Player No]
-    def UpdateForward(self,OppoMove,Player) :
+    def UpdateForward(self,Move,Player) :
         # Update Row State
-        self.BlockUtility += LineState.UtilityChangeForward[ self.Rows[OppoMove[0]] ][ OppoMove[1] ][ Player ]
-        self.Rows[OppoMove[0]] = LineState.ForwardAd[ self.Rows[OppoMove[0]] ][ OppoMove[1] ][ Player ]
+        self.BlockUtility += LineState.UtilityChangeForward[ self.Rows[Move[0]] ][ Move[1] ][ Player ]
+        self.Rows[Move[0]] = LineState.ForwardAd[ self.Rows[Move[0]] ][ Move[1] ][ Player ]
 
         # Update Col State
-        self.BlockUtility += LineState.UtilityChangeForward[ self.Cols[OppoMove[1]] ][ OppoMove[0] ][ Player ]
-        self.Cols[OppoMove[1]] = LineState.ForwardAd[ self.Cols[OppoMove[1]] ][ OppoMove[0] ][ Player ]
+        self.BlockUtility += LineState.UtilityChangeForward[ self.Cols[Move[1]] ][ Move[0] ][ Player ]
+        self.Cols[Move[1]] = LineState.ForwardAd[ self.Cols[Move[1]] ][ Move[0] ][ Player ]
 
         # if this lies on diagonal 0
-        if OppoMove[0] == OppoMove[1] :
-            self.BlockUtility += LineState.UtilityChangeForward[ self.Diags[0] ][ OppoMove[0] ][ Player ]
-            self.Diags[0] = LineState.ForwardAd[ self.Diags[0] ][ OppoMove[0] ][ Player ]
+        if Move[0] == Move[1] :
+            self.BlockUtility += LineState.UtilityChangeForward[ self.Diags[0] ][ Move[0] ][ Player ]
+            self.Diags[0] = LineState.ForwardAd[ self.Diags[0] ][ Move[0] ][ Player ]
 
         # if this lies on diagonal 1
-        if OppoMove[0] + OppoMove[1] == 3 :
-            self.BlockUtility += LineState.UtilityChangeForward[ self.Diags[1] ][ OppoMove[0] ][ Player ]
-            self.Diags[1] = LineState.ForwardAd[ self.Diags[0] ][ OppoMove[0] ][ Player ]
+        if Move[0] + Move[1] == 3 :
+            self.BlockUtility += LineState.UtilityChangeForward[ self.Diags[1] ][ Move[0] ][ Player ]
+            self.Diags[1] = LineState.ForwardAd[ self.Diags[0] ][ Move[0] ][ Player ]
 
         # Decrement the number of Empty cells
         self.EmptyCells -= 1
+
+
+    def UpdateBackward(self,Move,Player) :
+        # Update Row State
+        self.BlockUtility += LineState.UtilityChangeBackward[ self.Rows[Move[0]] ][ Move[1] ][ Player ]
+        self.Rows[Move[0]] = LineState.BackwardAd[ self.Rows[Move[0]] ][ Move[1] ][ Player ]
+
+        # Update Col State
+        self.BlockUtility += LineState.UtilityChangeBackward[ self.Cols[Move[1]] ][ Move[0] ][ Player ]
+        self.Cols[Move[1]] = LineState.BackwardAd[ self.Cols[Move[1]] ][ Move[0] ][ Player ]
+
+        # if this lies on diagonal 0
+        if Move[0] == Move[1] :
+            self.BlockUtility += LineState.UtilityChangeBackward[ self.Diags[0] ][ Move[0] ][ Player ]
+            self.Diags[0] = LineState.BackwardAd[ self.Diags[0] ][ Move[0] ][ Player ]
+
+        # if this lies on diagonal 1
+        if Move[0] + Move[1] == 3 :
+            self.BlockUtility += LineState.UtilityChangeBackward[ self.Diags[1] ][ Move[0] ][ Player ]
+            self.Diags[1] = LineState.BackwardAd[ self.Diags[0] ][ Move[0] ][ Player ]
+
+        # Decrement the number of Empty cells
+        self.EmptyCells += 1

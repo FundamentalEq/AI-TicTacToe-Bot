724c2f5e3360d035d39bf6c95e29cddce68d03b8
FundamentalEq <bloodspain@gmail.com>
2017-03-03 15:24:41 +0530

    Added missing functions

--- Board.py
+++ Board.py
@@ -12,13 +12,32 @@ class Board() :
         # 4x4 blocks on the board
         self.Blocks = [ Block.Block() for block in range(16) ]
 
-        # Current BoardUtility
-        self.BoardUtility = 1
-        # for block in self.Blocks :
-        #     self.BoardUtility += block.BlockUtility
+        self.Normalize = 10000
+        self.Rows = [ 0  for row in range(4) ]
+        self.Cols = [ 0  for col in range(4) ]
+        self.Diags = [ 0 for diag in range(4) ]
+        self.Index = [ (blockno//4,blockno%4) for block in range(16) ]
 
-        # Current Board
-        # self.Board = [ self.Empty for col in range(16) for row in range(16) ]
+        self.RowS = [ [0,0,0] for row in range(4) ]
+        self.ColS = [ [0,0,0] for col in range(4) ]
+        self.DiagS = [ [0,0,0] for diag in range(4) ]
+        self.WinNu = 0
+        self.LoseNu = 1
+        self.DrawNu = 2
+        self.Weights = [ 5 , 3 , 3 , 5 , 3 , 7  , 7 , 3 , 3 , 7 , 7 , 3 , 5 , 3 , 3 , 5 ]
+
+        for block in range(16) :
+            self.Rows[self.Index[block][0]] += self.Blocks[block].BlockUtility
+            self.Cols[self.Index[block][1]] += self.Blocks[block].BlockUtility
+            if self.Index[block][0] == self.Index[block][1] :
+                self.Diags[0] += self.Blocks[block].BlockUtility
+            if self.Index[block][0] + self.Index[block][1] == 3 :
+                self.Diags[1] +=  self.Blocks[block].BlockUtility
+
+        for row in range(4) :
+
+        # Current BoardUtility
+        self.BoardUtility = 0
 
         # WasteMove / NoMove
         self.WasteMove = (-1,-1)
@@ -35,9 +54,7 @@ class Board() :
         BlockCol = EnemyMove[1] % 4
 
         self.Blocks[BlockNo].UpdateForward((BlockRow,BlockCol),self.EnemyNu)
-        # **********************************************
-        # Need to implement change of the Board Utility
-        # **********************************************
+        self.MakeMove(BlockNo,(BlockRow,BlockCol),self.EnemyNu)
 
         return 4*BlockRow + BlockCol
 
@@ -52,36 +69,174 @@ class Board() :
 
         # We know the block where we are supposed to move
 
-    def CheckGameOver() :
-        pass
+    def CheckGameOver(self) :
+        Draws = 0
+        for row in range(4) :
+            if self.RowS[row][self.WinNu] == 4 or self.RowS[row][self.LoseNu] == 4 :
+                return True
+            Draws += self.RowS[row][self.DrawNu]
+        for col in range(4) :
+            if self.ColS[col][self.WinNu] == 4 or self.Cols[col][self.LoseNu] == 4 :
+                return True
+            Draws +=  self.RowS[self.DrawNu]
+        for diag in range(2) :
+            if self.DiagS[diag][self.WinNu] == 4 or self.DiagS[diag][self.LoseNu] == 4 :
+                return True
+            Draws += self.DiagS[diag][self.DrawNu]
+        if Draws == 16 :
+            return True
+        return False
 
-    def UpdateBoardUtility() :
-        pass
+    def FindBoardUtility(self) :
+        ans = 0
+        # Board Based Utility
+        for row in range(4) :
+            if (self.RowS[row][self.WinNu] == 0 or self.RowS[row][self.LoseNu] == 0 ) and self.RowS[row][self.DrawNu] == 0 :
+                ans += self.NUtility(self.Rows[row])
+        for col in range(4) :
+            if (self.ColS[col][self.WinNu] == 0 or self.Cols[col][self.LoseNu] == 0 ) and self.ColS[col][self.DrawNu] == 0 :
+                ans += self.NUtility(self.Cols[col])
 
-    def alpabeta(self,depth,alpha,beta,player,BlockNo) :
+        for diag in range(2) :
+            if (self.DiagS[diag][self.WinNu] == 0 or self.DiagS[diag][self.LoseNu] == 0 ) and self.DiagS[diag][self.DrawNu] == 0:
+                ans += self.NUtility(self.Diags[diag])
+
+        ans2 = 0
+        # WeightedSUm
+        for block in range(16) :
+            ans2 += self.Weights[block] * self.Blocks[block].BlockUtility
+        ans2 = float(ans2)/float(10)
+
+        return ans + ans2
+    def NUtility(self,val) :
+        if val > 0 :
+            sign = 1
+        else :
+            sign = -1
+        return sign * 10**(float(abs(val))/float(self.Normalize))
+
+    def MakeMove(self,BlockNo,Move,Player) :
+        PrevVal = self.Blocks[BlockNo].BlockUtility
+
+        self.Blocks[BlockNo].UpdateForward(Move,Player)
+
+        if self.Blocks[BlockNo].Status == "Won" :
+            self.RowS[self.Index[BlockNo][0]][self.WinNu] += 1
+            self.ColS[self.Index[BlockNo][1]][self.WinNu] += 1
+
+            if self.Index[BlockNo][0] == self.Index[BlockNo][1] :
+                self.DiagS[0][self.WinNu] += 1
+
+            if self.Index[BlockNo][0] + self.Index[BlockNo][1]  == 3:
+                self.DiagS[1][self.WinNu] += 1
+
+        elif self.Blocks[BlockNo].Status == "Lost" :
+            self.RowS[self.Index[BlockNo][0]][self.LoseNu] += 1
+            self.ColS[self.Index[BlockNo][1]][self.LoseNu] += 1
+
+            if self.Index[BlockNo][0] == self.Index[BlockNo][1] :
+                self.DiagS[0][self.LoseNu] += 1
+
+            if self.Index[BlockNo][0] + self.Index[BlockNo][1]  == 3:
+                self.DiagS[1][self.LoseNu] += 1
+
+        elif self.Blocks[BlockNo].EmptyCells == 0 :
+            self.RowS[self.Index[BlockNo][0]][self.DrawNu] += 1
+            self.ColS[self.Index[BlockNo][1]][self.DrawNu] += 1
+
+            if self.Index[BlockNo][0] == self.Index[BlockNo][1] :
+                self.DiagS[0][self.DrawNu] += 1
+
+            if self.Index[BlockNo][0] + self.Index[BlockNo][1]  == 3:
+                self.DiagS[1][self.DrawNu] += 1
+
+
+        Change = self.Blocks[BlockNo].BlockUtility - PrevVal
+
+        self.Rows[self.Index[BlockNo][0]] += Change
+        self.Cols[self.Index[BlockNo][1]] += Change
+
+        # is on Diagonal0
+        if self.Index[BlockNo][0] == self.Index[BlockNo][1] :
+            self.Diags[0] += Change
+
+        # is on Diagonal1
+        if self.Index[BlockNo][0] + self.Index[BlockNo][1] == 3 :
+            self.Diags[1] += Change
+
+
+    def UndoMove(self,BlockNo,Move,Player) :
+        PrevVal = self.Blocks[BlockNo].BlockUtility
+
+        if self.Blocks[BlockNo].Status == "Won" :
+            self.RowS[self.Index[BlockNo][0]][self.WinNu] -= 1
+            self.ColS[self.Index[BlockNo][1]][self.WinNu] -= 1
+
+            if self.Index[BlockNo][0] == self.Index[BlockNo][1] :
+                self.DiagS[0][self.WinNu] -= 1
+
+            if self.Index[BlockNo][0] + self.Index[BlockNo][1]  == 3:
+                self.DiagS[1][self.WinNu] -= 1
+
+        elif self.Blocks[BlockNo].Status == "Lost" :
+            self.RowS[self.Index[BlockNo][0]][self.LoseNu] -= 1
+            self.ColS[self.Index[BlockNo][1]][self.LoseNu] -= 1
+
+            if self.Index[BlockNo][0] == self.Index[BlockNo][1] :
+                self.DiagS[0][self.LoseNu] -= 1
+
+            if self.Index[BlockNo][0] + self.Index[BlockNo][1]  == 3:
+                self.DiagS[1][self.LoseNu] -= 1
+
+        elif self.Blocks[BlockNo].EmptyCells == 0 :
+            self.RowS[self.Index[BlockNo][0]][self.DrawNu] -= 1
+            self.ColS[self.Index[BlockNo][1]][self.DrawNu] -= 1
+
+            if self.Index[BlockNo][0] == self.Index[BlockNo][1] :
+                self.DiagS[0][self.DrawNu] -= 1
+
+            if self.Index[BlockNo][0] + self.Index[BlockNo][1]  == 3:
+                self.DiagS[1][self.DrawNu] -= 1
+
+
+        self.Blocks[BlockNo].UpdateBackward(Move,Player)
+        Change = self.Blocks[BlockNo].BlockUtility - PrevVal
+
+        self.Rows[self.Index[BlockNo][0]] += Change
+        self.Cols[self.Index[BlockNo][1]] += Change
+
+        # is on Diagonal0
+        if self.Index[BlockNo][0] == self.Index[BlockNo][1] :
+            self.Diags[0] += Change
+
+        # is on Diagonal1
+        if self.Index[BlockNo][0] + self.Index[BlockNo][1] == 3 :
+            self.Diags[1] += Change
+
+
+    def alphabetaRunner(self,depth,alpha,beta,playe,BlockNo) :
+
+    def alphabeta(self,depth,alpha,beta,player,BlockNo) :
 
         if depth == 0 or self.CheckGameOver() :
-            return self.BoardUtility
+            return self.FindBoardUtility()
 
         # order the moves in the required order
-        if self.BlockAvailable(BlockNo) :
+        if self.Blocks[BlockNo].Status == "War" :
 
             MoveSet = OrderMoves()
 
-            ret = self.Blocks[ BlockNo ].UpdateForward( MoveSet[0] , player )
-            self.BoardUtility *= ret
+            self.MakeMove(BlockNo,MoveSet[0],player)
 
             # For the thought best move do the complete search
             current = -self.alpabeta(depth - 1,-beta,-alpha,player^1,self.FindNextBlock(BlockNo,MoveSet[0]))
             BestMove = MoveSet[0]
 
-            ret = self.Blocks[ BlockNo ].UpdateBackward( MoveSet[0] , player)
-            self.BoardUtility *= ret
+            self.UndoMove(BlockNo,MoveSet[0],player)
 
             for i in range(1,len(MoveSet)) :
                 # Play according to assumption
-                ret = self.Blocks[ BlockNo ].UpdateForward( MoveSet[i] , player )
-                self.BoardUtility *= ret
+                self.MakeMove(Blocks,MoveSet[i],player)
 
                 score = -self.alpabeta(depth - 1,-alpha-1,-alpha,player^1,self.FindNextBlock(BlockNo,MoveSet[0]))
 
@@ -89,8 +244,7 @@ class Board() :
                 if score > alpha and score < beta :
                     score = -alpabeta(depth - 1,-beta,-alpha,player^1,self.FindNextBlock(BlockNo,MoveSet[0]))
 
-                ret = self.Blocks[ BlockNo ].UpdateBackward( MoveSet[0] , player)
-                self.BoardUtility *= ret
+                self.UndoMove(Blocks,MoveSet[i],player)
 
                 if score >= current :
                     current = score
